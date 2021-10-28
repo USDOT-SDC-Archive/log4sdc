d9ad25dcbe0298e47219ecd6cc693559e8192d98
kjaligama123 <krishna.jaligama.ctr@dot.gov>
2021-10-28 23:59:16 +0000

    updates to the deploy script

--- lambda-layer/deploy/deploy-common-layer.sh
+++ lambda-layer/deploy/deploy-common-layer.sh
@@ -6,8 +6,11 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # /python/lib/python3.7/site-packages
 
 ZIP_FILE=log4sdc-common.zip
-LAMBDA_LAYER=log4sdc-common
+LAMBDA_LAYER=log4sdc
 WORK_DIR=$SCRIPT_DIR
+
+# replace with the actual bucket name for deployment
+S3_LAMBDA_BUCKET=lambda_binary_bucket
 
 #echo zip -r $WORK_DIR/log4sdc.zip $WORK_DIR/src/*
 
@@ -31,15 +34,15 @@ zip -r $WORK_DIR/$ZIP_FILE python
 
 cd $CURR_DIR
 
-aws s3 cp $WORK_DIR/$ZIP_FILE s3://dev-lambda-bucket-505135622787/log4sdc/ --sse
-echo https://s3.amazonaws.com/dev-lambda-bucket-505135622787/log4sdc/$ZIP_FILE
+aws s3 cp $WORK_DIR/$ZIP_FILE s3://$S3_LAMBDA_BUCKET/log4sdc/ --sse
+echo https://s3.amazonaws.com/$S3_LAMBDA_BUCKET/log4sdc/$ZIP_FILE
 
 ###aws lambda update-function-code --function-name  $LAMBDA_LAYER --zip-file fileb://$WORK_DIR/$ZIP_FILE
-#aws lambda update-function-code --function-name  v-test --zip-file https://s3.amazonaws.com/dev-lambda-bucket-505135622787/log4sdc/log4sdc.zip
+#aws lambda update-function-code --function-name  v-test --zip-file https://s3.amazonaws.com/$S3_LAMBDA_BUCKET/log4sdc/log4sdc.zip
 
 aws lambda publish-layer-version \
     --layer-name $LAMBDA_LAYER \
     --description "$LAMBDA_LAYER" \
-    --content S3Bucket=dev-lambda-bucket-505135622787,S3Key=log4sdc/$ZIP_FILE \
+    --content S3Bucket=$S3_LAMBDA_BUCKET,S3Key=log4sdc/$ZIP_FILE \
     --compatible-runtimes python3.7 python3.8 python3.9
 
